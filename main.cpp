4223a4310ee107d760a7b8054b52a84983dbd98d
Fredrik Lind <fredrik@editerat.se>
2013-11-04 14:57:46 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab0
+//
+//  Created by Fredrik Lind on 2013-11-04.
+//  Copyright (c) 2013 Fredrik Lind. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
